1dbcaf516d5836d99b6ebfcf618c999c5b111c20
Bell Eapen <github_public@gulfdoctor.net>
2024-10-09 15:09:36 +0000

    Refactor llm-cql.py script to convert CQL queries to natural language questions

--- llm-cql.py
+++ llm-cql.py
@@ -7,21 +7,25 @@ import pandas as pd
 model_id = "microsoft/Phi-3-mini-128k-instruct"
 tokenizer = AutoTokenizer.from_pretrained(model_id)
 model = AutoModelForCausalLM.from_pretrained(model_id, device_map="auto")
-pipe = pipeline("text-generation", model=model, tokenizer=tokenizer, max_new_tokens=64)
+pipe = pipeline("text-generation", model=model, tokenizer=tokenizer, max_new_tokens=16)
 llm = HuggingFacePipeline(pipeline=pipe)
 
 assertions = pd.read_csv('map/diagnosis.csv')
 
 MAP_TEMPLATE = """
-You are an assistant that can map a CQL query to a natural language question.
-You will be given a CQL query. You have to answer with a natural language question as in the example below.
+You are an assistant that can convert a CQL query to a natural language.
+You should give a single line answer>> as in the example below.
 Example:
 
-CQL Query: exists (["DocumentReference": "Diabetes Mellitus"])
-Natural Language Question: Does the document ascertain Diabetes Mellitus?
+CQL: exists (["DocumentReference": "Diabetes Mellitus"])
+answer>> Does the document ascertain Diabetes Mellitus?
+CQL: exists (["DocumentReference": "Herpes Zoster"])
+answer>> Does the document ascertain Herpes Zoster?
 
-CQL Query: exists (["DocumentReference": {diagnosis}])
-Natural Language Question:: """
+Now convert the following CQL query to a natural language.
+CQL: exists (["DocumentReference": {diagnosis}])
+
+answer>> """
 
 map_prompt = PromptTemplate.from_template(MAP_TEMPLATE)
 
@@ -31,5 +35,5 @@ for i, row in assertions.iterrows():
     }
     chain = map_prompt | llm | StrOutputParser()
     response = chain.invoke(chunk)
-    question = response.split("::")[-1].strip()
+    question = response.split(">>")[-1].strip()
     print(question)
