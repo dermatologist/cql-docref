270d50828818242db8e12c98edcb1e7fc43c88eb
Bell Eapen <github_public@gulfdoctor.net>
2024-10-09 15:50:02 +0000

    Refactor llm-cql.py script to convert CQL queries to natural language questions and add functionality to assert diagnoses in discharge notes

--- llm-cql.py
+++ llm-cql.py
@@ -2,7 +2,9 @@ from langchain_huggingface.llms import HuggingFacePipeline
 from transformers import AutoModelForCausalLM, AutoTokenizer, pipeline
 from langchain_core.prompts import PromptTemplate
 from langchain_core.output_parsers import StrOutputParser
+from langchain_text_splitters import CharacterTextSplitter
 import pandas as pd
+
 
 model_id = "microsoft/Phi-3-mini-128k-instruct"
 tokenizer = AutoTokenizer.from_pretrained(model_id)
@@ -11,6 +13,7 @@ pipe = pipeline("text-generation", model=model, tokenizer=tokenizer, max_new_tok
 llm = HuggingFacePipeline(pipeline=pipe)
 
 assertions = pd.read_csv('map/diagnosis.csv')
+
 
 MAP_TEMPLATE = """
 You are an assistant that can convert a CQL query to a natural language.
@@ -29,11 +32,59 @@ answer>> """
 
 map_prompt = PromptTemplate.from_template(MAP_TEMPLATE)
 
-for i, row in assertions.iterrows():
-    chunk = {
-        "diagnosis": row["diagnosis"]
-    }
-    chain = map_prompt | llm | StrOutputParser()
-    response = chain.invoke(chunk)
-    question = response.split(">>")[-1].strip()
-    print(question)
+ASSERT_TEMPLATE = """
+You will be given a document and the concept. You have to answer with a yes or no.
+document: {document}
+{question} yes or no: """
+
+assert_prompt = PromptTemplate.from_template(ASSERT_TEMPLATE)
+
+
+# data = []
+# for i, row in assertions.iterrows():
+#     chunk = {
+#         "diagnosis": row["diagnosis"]
+#     }
+#     chain = map_prompt | llm | StrOutputParser()
+#     response = chain.invoke(chunk)
+#     question = response.split(">>")[-1].strip()
+#     data.append([row['subject_id'], row['diagnosis'], question])
+#     print(question)
+# _df = pd.DataFrame(data, columns=['subject_id', 'diagnosis', 'question'])
+# _df.to_csv('map/diagnosis_questions.csv', index=False)
+
+
+questions = pd.read_csv('map/diagnosis_questions.csv')
+main_data = pd.read_csv('map/discharge_sample.csv')
+
+def chunk_notes(subject_id):
+    docs = []
+    for index, notes in main_data[main_data['subject_id'] == subject_id].iterrows():
+        discharge_note = notes['text']
+        text_splitter = CharacterTextSplitter.from_tiktoken_encoder(
+            chunk_size=1280, chunk_overlap=5
+        )
+        split_docs = text_splitter.split_text(discharge_note)
+        for doc in split_docs:
+            docs.append(doc)
+    return docs
+
+def assert_diagnosis(docs, question):
+    for doc in docs:
+        chunk = {
+            "document": doc,
+            "question": question
+        }
+        chain = assert_prompt | llm | StrOutputParser()
+        answer = chain.invoke(chunk).split(" ")[-1].lower().strip()
+        if answer == "yes":
+            return True
+    return False
+
+for i, row in questions.iterrows():
+    subject_id = row['subject_id']
+    question = row['question']
+    docs = chunk_notes(subject_id)
+    print(f"Subject ID: {subject_id}, Question: {question}")
+    print(f"Answer: {assert_diagnosis(docs, question)}")
+
