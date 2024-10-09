1cbe9e0bddf03b83ceab830a408df8b5e63e8956
Bell Eapen <github_public@gulfdoctor.net>
2024-10-09 21:39:33 +0000

    Refactor llm-cql.py script to convert CQL queries to natural language questions, optimize memory usage, and update text splitting chunk size

--- llm-cql.py
+++ llm-cql.py
@@ -32,7 +32,7 @@ CQL:     exists (
         and D.findings="Intact sensation to light touch"
         )
 
-answer>> Does the document mention Penicillin allergy, Headache, Weakness, Numbness, and Intact sensation to light touch as findings?
+answer>> Penicillin allergy, Headache, Weakness, Numbness, and Intact sensation to light touch as findings?
 
 CQL:    exists (
             [DocumentReference] D
@@ -42,7 +42,7 @@ CQL:    exists (
             and not D.finding="Fluid collection"
         )
 
-answer>> Does the document mention Diverticulitis diagnosis, Fever complaint, Colon resection procedure, and no Fluid collection finding?
+answer>> Diverticulitis diagnosis, Fever complaint, Colon resection procedure, and no Fluid collection finding?
 
 Now convert the following CQL query to a natural language.
 CQL: {cql}
@@ -53,12 +53,19 @@ map_prompt = PromptTemplate.from_template(MAP_TEMPLATE)
 
 ASSERT_TEMPLATE = """
 You will be given a document and a question.\n
-You have to answer the question with a yes or no.
+Extract ONLY the relevant lines from the document related to the statement: {question} \n
 document: {document} \n
-{question} yes or no: """
+Relevant lines:: """
 
 assert_prompt = PromptTemplate.from_template(ASSERT_TEMPLATE)
 
+FINAL_TEMPLATE = """
+You will be given a document and a question.\n
+You have to answer the question with a yes or no.
+document: {document} \n
+Does the document mention {question} yes or no: """
+
+final_prompt = PromptTemplate.from_template(FINAL_TEMPLATE)
 
 data = []
 for i, row in assertions.iterrows():
@@ -74,12 +81,9 @@ _df = pd.DataFrame(data, columns=['subject_id', 'cql', 'question'])
 _df.to_csv('map/diagnosis_questions.csv', index=False)
 
 
-questions = pd.read_csv('map/diagnosis_questions.csv')
-main_data = pd.read_csv('map/discharge_sample.csv')
-
-def chunk_notes(subject_id):
+def chunk_notes(data):
     docs = []
-    for index, notes in main_data[main_data['subject_id'] == subject_id].iterrows():
+    for index, notes in data.iterrows():
         discharge_note = notes['text']
         text_splitter = CharacterTextSplitter.from_tiktoken_encoder(
             chunk_size=1024, chunk_overlap=5
@@ -89,25 +93,41 @@ def chunk_notes(subject_id):
             docs.append(doc)
     return docs
 
-def assert_diagnosis(docs, question):
-    torch.cuda.empty_cache()
+def final_answer(facts, question):
+    print(f"facts: {facts}")
+    chunk = {
+        "document": facts,
+        "question": question
+    }
+    chain = final_prompt | llm | StrOutputParser()
+    answer = chain.invoke(chunk)
+    if "yes" in answer.lower():
+        return True
+    return False
+
+def collect_facts(docs, question):
+    facts = ""
     for doc in tqdm.tqdm(docs):
         chunk = {
             "document": doc,
             "question": question
         }
         chain = assert_prompt | llm | StrOutputParser()
-        answer = chain.invoke(chunk).split(" ")[-1].lower().strip()
-        if "yes" in answer.lower():
-            return True
-    return False
+        answer = chain.invoke(chunk).split("::")[-1].strip()
+        # print(f"Answer: {answer}")
+        facts += answer + " "
+    return facts
 
+questions = pd.read_csv('map/diagnosis_questions.csv')
+main_data = pd.read_csv('map/discharge_sample.csv')
 for i, row in questions.iterrows():
     subject_id = row['subject_id']
     question = row['question']
-    docs = chunk_notes(subject_id)
+    data = main_data[main_data['subject_id'] == subject_id]
+    docs = chunk_notes(data)
     if len(docs) > 20:
         continue
-    answer = assert_diagnosis(docs, question)
+    facts = collect_facts(docs, question)
+    answer = final_answer(facts, question)
     print(f"Subject ID: {subject_id}, Question: {question}, Answer: {answer}")
 
